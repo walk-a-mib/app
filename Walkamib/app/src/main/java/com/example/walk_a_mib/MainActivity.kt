466d762544894c354976cba4ff3117729e85d6f8
Andrea Mauriello <andrea.mauriello16@gmail.com>
2022-11-25 12:40:48 +0100

    added horizontalScrollView

--- Walkamib/app/src/main/java/com/example/walk_a_mib/MainActivity.kt
+++ Walkamib/app/src/main/java/com/example/walk_a_mib/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.walk_a_mib
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val imgBtn1 = findViewById<ImageButton>(R.id.imgBtn1)
+
+        imgBtn1.setOnClickListener {
+            Snackbar.make(imgBtn1, "Lorem ipsum dolor sit amet", Snackbar.LENGTH_LONG).show()
+        }
     }
 }
