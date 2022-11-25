8791a49cee9fe596fc6be8712cad960d76efa8c0
Andrea Mauriello <andrea.mauriello16@gmail.com>
2022-11-25 11:49:35 +0100

    app creation

---
+++ Walkamib/app/src/main/java/com/example/walk_a_mib/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.walk_a_mib
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
