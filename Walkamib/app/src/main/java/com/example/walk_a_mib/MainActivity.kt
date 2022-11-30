d4e86e0a7f03b7948ff051ba2dc4b37021eb023b
Andrea Mauriello <andrea.mauriello16@gmail.com>
2022-11-30 01:42:20 +0100

    created bottomsheet

--- Walkamib/app/src/main/java/com/example/walk_a_mib/MainActivity.kt
+++ Walkamib/app/src/main/java/com/example/walk_a_mib/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.walk_a_mib
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageButton
 import android.widget.ImageView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,13 +16,17 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val settings = findViewById<ImageView>(R.id.settings)
-
         settings.setOnClickListener {
             startActivity(Intent(this, SettingsActivity::class.java))
             overridePendingTransition(
                 R.anim.slide_in_top,
                 R.anim.slide_out_bottom
             )
+        }
+
+        BottomSheetBehavior.from(sheet).apply {
+            peekHeight = 200
+            this.state = BottomSheetBehavior.STATE_COLLAPSED
         }
     }
 }
