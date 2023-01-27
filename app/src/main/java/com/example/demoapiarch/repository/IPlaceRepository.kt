33bc7521ac3a1377ce621c4ca8ee2fb1e6135d98
FerraChris01 <ferrareis.christian@gmail.com>
2023-01-27 11:25:53 +0100

    utilizzo annotationi @Serialize

--- app/src/main/java/com/example/demoapiarch/repository/IPlaceRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.demoapiarch.repository
-
-import androidx.lifecycle.MutableLiveData
-import com.example.demoapiarch.place.PlaceResult
-
-
-interface IPlaceRepository {
-    fun fetchPlace(placeId: String, lastUpdate: Long): MutableLiveData<PlaceResult>?
-}
--- app/src/main/java/com/example/demoapiarch/source/BasePlaceRemoteDataSource.kt
+++
@@ -1,7 +0,0 @@
-package com.example.demoapiarch.source
-
-abstract class BasePlaceRemoteDataSource {
-    var placeCallback : PlaceCallback? = null
-
-    abstract fun getPlace(placeId : String)
-}
